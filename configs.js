7d7ec31f2701cb9e3534def57f8ccff03093dd94
Airscript <francesco@airscript.it>
2023-01-21 16:24:11 +0100

    feat: add missing configs.js file

---
+++ configs.js
@@ -0,0 +1,10 @@
+export const configs = {
+  impi: null,
+  impu: null,
+  realm: null,
+  password: null,
+  ice_servers: null,
+  display_name: null,
+  websocket_proxy_url: null,
+  enable_rtcweb_breaker: null,
+};
